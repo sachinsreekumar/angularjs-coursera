ecf420a95e7458aa7f6257641886a8691880c327
sachin <sachinsreekumar2@gmail.com>
2020-04-15 23:06:48 +0530

    Completed lecture9

--- Lecture09/beginning-of-lecture-code/app.js
+++ Lecture09/beginning-of-lecture-code/app.js
@@ -4,8 +4,14 @@
 angular.module('DIApp', [])
 .controller('DIController', DIController);
 
-function DIController ($scope) {
+function DIController ($scope, $filter) {
   $scope.name = "Yaakov";
+
+  $scope.upper = function () {
+    var upCase = $filter('uppercase');
+    $scope.name = upCase($scope.name);
+  };
+
 }
 
 })();
